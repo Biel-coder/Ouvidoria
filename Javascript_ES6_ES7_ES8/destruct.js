e88ae8ec9cb9fdf2f106f9276453e3d560f0ef37
Biel-coder <biel.s.maia@hotmail.com>
2020-05-07 20:53:30 -0300

    Estudo de Nodejs

---
+++ Javascript_ES6_ES7_ES8/destruct.js
@@ -0,0 +1,10 @@
+var user = {
+    nome: "Gabriel Silva",
+    idade: 20,
+    atuacao: "Estudante",
+    curso: "Ciência da Computação"
+}
+
+var { nome,idade,atuacao,curso } = user;
+
+console.log(curso)
